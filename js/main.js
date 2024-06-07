4b7ec6b35dd033f9408e73dff19308c70ad73de9
doaamahmoud930 <doaamahmoud930@gmail.com>
2024-06-07 21:20:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,119 @@
+var taskName = document.getElementById("taskName");
+var addInput = document.getElementById("addInput");
+var alertName = document.getElementById("alertName");
+var btnUpdate = document.getElementById("btnUpdate");
+var filterTasks = document.getElementById("filterTasks");
+var taskContainer;
+var currentIndex = -1;
+//////////////////////////////////////////////////////////////////////////
+// show local storage contains
+ if(localStorage.getItem('ourTasks')){
+   var taskContainer = JSON.parse(localStorage.getItem('ourTasks'));
+
+ }else{
+    taskContainer = [];
+ }
+////////////////////////////////////////////////////////////////////////
+/// Add new Task.
+function addTask(){
+    task = {
+        name:taskName.value,
+        completed: false
+    }
+    taskContainer.push(task);
+    localStorage.setItem('ourTasks',JSON.stringify(taskContainer));
+    console.log(taskContainer);
+    clearInput();
+    addInput.removeAttribute("disabled"); 
+}
+//////////////////////////////////////////////////////////
+function clearInput(){
+    taskName.value="";
+}
+////////////////////////////////////////////////////////////////////////
+//Display all Tasks
+// function displayTasks(){
+//     var table =``;
+//     var filter = filterTasks.value;
+//     for(var i=0 ; i<taskContainer.length ; i++){
+//         let taskClass = taskContainer[i].completed ? 'completed' : 'uncomplete';
+//         if (filter === 'completed' && !taskContainer[i].completed) continue;
+//         if (filter === 'uncompleted' && taskContainer[i].completed) continue;
+//         table +=`
+//         <div class="col-10">
+//         <div class="input-group mb-3 ">   
+//             <div class="border p-1 w-50 bg-light rounded form-control-lg ${taskClass}">${taskContainer[i].name}</div>
+//            <button class="btn btn-outline-secondary" type="button" onclick='completeTask(${i})'><i class="fas fa-check text-success"></i></button>
+//            <button class="btn btn-outline-secondary" type="button" onclick='updateTask(${i})'><i class="fas fa-edit text-primary"></i></i></button>
+//             <button class="btn btn-outline-secondary" type="button" onclick='deleteTask(${i})'><i class="fas fa-trash text-danger"></i></button>
+//           </div>
+//     </div>`;
+
+//     }
+//     document.getElementById("taskBody").innerHTML=table;
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////
+// function updateTask(index){
+//     currentIndex = index;
+//     taskName.value = taskContainer[index].name;
+//     addInput.classList.add('d-none');
+//     btnUpdate.classList.remove('d-none');
+// }
+
+// btnUpdate.addEventListener('click' ,(e) => {
+//     e.preventDefault();
+//     addUpdate();
+// });
+
+// function addUpdate(){
+//     taskContainer[currentIndex].name = inputName.value;
+//     addInput.classList.add('d-none');
+//     btnUpdate.classList.remove('d-none');
+//     localStorage.setItem('ourTasks', JSON.stringify(taskContainer));
+//     displayTasks();
+//     clearInput();
+
+// }
+//////////////////////////////////////////////////////////////////
+
+///////////Validate Task.
+// function validateTask(){
+//     var regexName =/^[a-z A-Z]{3,}/;
+//     if (regexName.test(taskName.value) == true){
+//         alertName.classList.remove("d-block");
+//         alertName.classList.add("d-none");
+//         addInput.removeAttribute("disabled");
+//         btnUpdate.removeAttribute("disabled");
+//         return true;
+//     }
+//     else{
+//         alertName.classList.remove("d-none");
+//         alertName.classList.add("d-block");
+//         addInput.setAttribute("disabled", "true");
+//         btnUpdate.setAttribute("disabled", "true");
+//         return false;
+//     }
+
+// }
+
+// taskName.addEventListener("blur", validateTask);
+/////////////////////////////////////////////////////////////////////////
+//   Delete specific Task.
+// function deleteTask(index){
+//     taskContainer.splice(index , 1);
+//     localStorage.setItem('ourTasks' , JSON.stringify(taskContainer));
+//     displayTasks();
+    
+//     }
+
+/////////////////////////////////////
+//Complete Task
+// function completeTask(index){
+//     taskContainer[index].completed = !taskContainer[index].completed;
+//     localStorage.setItem('ourTasks', JSON.stringify(taskContainer));
+//     displayTasks();
+// }
+
+// filterTasks.addEventListener('change', displayTasks);
